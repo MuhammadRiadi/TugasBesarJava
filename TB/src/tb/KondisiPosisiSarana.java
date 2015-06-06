f664ecdeed92c595f8b91adcc675ba7c44e42486
haisabdillah <haismansyahh@gmail.com>
2015-06-06 09:14:50 +0700

    Haismansyah Abdillah

--- TB/src/tb/KondisiPosisiSarana.java
+++ TB/src/tb/KondisiPosisiSarana.java
@@ -1,6 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To chls | Templates
  * and open the template in the editor.
  */
 package finaltb;
