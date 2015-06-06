f32ee46e184956bcdfe020fbe957876a1f2dcc1b
alexandersutardiantoro <alexander_sdt@yahoo.co.id>
2015-06-06 09:24:27 +0700

    Alexander Sutardiantoro

--- TB/src/tb/KeamananRuangKelas.java
+++ TB/src/tb/KeamananRuangKelas.java
@@ -3,47 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jack;
+package finaltb;
 
-import java.util.Scanner;
-public class KeamananRuangKelas {
-    Scanner scan=new Scanner(System.in);
-    private int kekokohan;
-    private int kunci;
-    private int keamanan;
-    
 
-    public int getKekokohan() {
-        return kekokohan;
+public class KeamananRuangKelas extends AnalisisKeamananRuangKelas {
+
+    public KeamananRuangKelas(int kekokohan, int kunci, int keamanan) {
+        super(kekokohan, kunci, keamanan);
     }
-
-    public void setKekokohan(int kekokohan) {
-        this.kekokohan = kekokohan;
-    }
-
-    public int getKunci() {
-        return kunci;
-    }
-
-    public void setKunci(int kunci) {
-        this.kunci = kunci;
-    }
-
-    public int getKeamanan() {
-        return keamanan;
-    }
-
-    public void setKeamanan(int keamanan) {
-        this.keamanan = keamanan;
-    }
-    
+   
+    @Override
      void Kekokohan(){
         System.out.println("\n\n==KEAMANAN RUANG KELAS==\n\n");
         System.out.println("Apakah Ruang Kelas Kokoh");
         System.out.println("1.Kokoh\n2.Tidak Kokoh\nPilih [1-2]");
         setKekokohan(scan.nextInt());
     }
-    
+    @Override
+             
      void Kuncipintujendela(){
         
         
@@ -51,7 +28,7 @@ public class KeamananRuangKelas {
         System.out.println("1.Ada\n2.Tidak Ada\nPilih [1-2]");
         setKunci(scan.nextInt());
     }
-    
+    @Override
      void Keamanan(){
         
         
@@ -61,6 +38,12 @@ public class KeamananRuangKelas {
         
         
     }
+     @Override
+     void OutputKeamanan(){
+         getKekokohan();
+         getKunci();
+         getKeamanan();
+     }
    
     
 }
