90f86ddc00f6710adb6eaa2f69805ec81eda0f0f
alexandersutardiantoro <alexander_sdt@yahoo.co.id>
2015-06-06 09:25:08 +0700

    Alexander Sutardiantoro

--- TB/src/tb/KebersihanRuangKelas.java
+++ TB/src/tb/KebersihanRuangKelas.java
@@ -1,70 +1,47 @@
 
-package jack;
-import java.util.Scanner;
-public class KebersihanRuangKelas {
-    Scanner scan=new Scanner(System.in);
-    private int udara;
-    private int cahaya;
-    private int kelembapan;
-    private int suhu;
+package finaltb;
 
-    public int getSuhu() {
-        return suhu;
-    }
+public class KebersihanRuangKelas extends AnalisisKebersihanRuangKelas {
+   
 
-    public void setSuhu(int suhu) {
-        this.suhu = suhu;
-    }
-    public int getCahaya() {
-        return cahaya;
-    }
-
-    public void setCahaya(int cahaya) {
-        this.cahaya = cahaya;
-    }
-
-    public int getUdara() {
-        return udara;
-    }
-
-    public void setUdara(int udara) {
-        this.udara = udara;
-    }
-
- 
-
-    public int getKelembapan() {
-        return kelembapan;
-    }
-
-    public void setKelembapan(int kelembapan) {
-        this.kelembapan = kelembapan;
+    public KebersihanRuangKelas(int udara, int cahaya, int kelembapan, int suhu) {
+        super(udara, cahaya, kelembapan, suhu);
     }
     
+    @Override
      void SirkulasiUdara(){
         System.out.println("\n\n==KEBERSIHAN RUANG KELAS==\n\n");
         System.out.println("Masukkan Sirkulasi Udara");
         System.out.println("1.Lancar\n2.Tidak Lancar\nPilih [1-2]");
-        setUdara(scan.nextInt());
+        super.setUdara(scan.nextInt());
           
         }
-        
+        @Override
      void Pencahayaan(){
         System.out.println("Masukkan Nilai Pencahayaan [250-350]");
-        setCahaya(scan.nextInt());
+        super.setCahaya(scan.nextInt());
         
      }
-      
+      @Override
      void Kelembapan(){
         System.out.println("Input Kelembapan [70%-80%]=");
-        setKelembapan(scan.nextInt());
+        super.setKelembapan(scan.nextInt());
        
      }
-        
+        @Override
      void Suhu(){
         System.out.println("Input Suhu [25-35]=");
-        setSuhu(scan.nextInt());
+        super.setSuhu(scan.nextInt());
  }
+     @Override
+     void OutputKebersihan(){
+         
+         super.getUdara();
+         super.getCahaya();
+         super.getKelembapan();
+         super.getSuhu();
+     }
+             
     
     
 }
