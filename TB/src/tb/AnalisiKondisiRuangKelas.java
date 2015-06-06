62c2d78fbb7ad3673642f87ce3d4a092904f2df7
haisabdillah <haismansyahh@gmail.com>
2015-06-06 08:25:40 +0700

    Update AnalisiKondisiRuangKelas.java

--- TB/src/tb/AnalisiKondisiRuangKelas.java
+++ TB/src/tb/AnalisiKondisiRuangKelas.java
@@ -3,51 +3,135 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hais;
+package finaltb;
 
-/**
- *
- * @author Predator
- */
-public class AnalisiKondisiRuangKelas extends KondisiRuangKelas {
+import java.util.Scanner;
+abstract public class AnalisiKondisiRuangKelas  {
+    private int panjang;
+    private int lebar;
+    private int luas;
+    private int jumlahkursi;
+    private int rasio;
+    private int pintu;
+    private int jendela;
+    Scanner scan=new Scanner(System.in);
+
+    public AnalisiKondisiRuangKelas(int panjang, int lebar, int luas, int jumlahkursi, int rasio, int pintu, int jendela) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+        this.luas = luas;
+        this.jumlahkursi = jumlahkursi;
+        this.rasio = rasio;
+        this.pintu = pintu;
+        this.jendela = jendela;
+    }
     
-     @Override
-    void Luas() {
-        super.Luas(); 
-        setLuas(getPanjang()*getLebar());
-        
-        System.out.println("Luas Kelas= "+getLuas());
-        if(getPanjang()==getLebar()){
-            System.out.println("Bentuk Ruang Persegi");
+
+    public int getPanjang() {
+        return panjang;
+    }
+
+    public void setPanjang(int panjang) {
+        this.panjang = panjang;
+    }
+
+    public int getLebar() {
+        return lebar;
+    }
+
+    public void setLebar(int lebar) {
+        this.lebar = lebar;
+    }
+
+    public int getLuas() {
+        luas = getPanjang()*getLebar();
+        System.out.print("Luas="+luas);
+        if(getPanjang()==getLebar()) {
+            System.out.println("/Bentuk Ruang Persegi");
         }
         else
             if(getPanjang()!=getLebar())
-        {System.out.println("Bentuk Ruang Persegi Panjang");
-    }
+        {   System.out.println("/Bentuk Ruang Persegi Panjang");
+        
+    }   return luas;
+}
+
+    
+
+    public int getJumlahkursi() {
+        return jumlahkursi;
     }
 
-    @Override
-    void Rasio() {
-        super.Rasio(); 
-         setRasio(getLuas()/getJumlahkursi());
-                if(getRasio() >= 0.5){
-                    System.out.println("Rasio Sesuai");
+    public void setJumlahkursi(int jumlahkursi) {
+        this.jumlahkursi = jumlahkursi;
+    }
+
+    public int getRasio() {
+        rasio=luas/getJumlahkursi();
+        System.out.print("Rasio:"+rasio);
+                if(rasio >= 0.5){
+                    System.out.println("/Sesuai");
                 }
                 else
-                    System.out.println("Rasio Tidak Sesuai");
+                    System.out.println("/Tidak Sesuai");
+        return rasio;
+        
     }
 
-    @Override
-    void pintujendela() {
-        super.pintujendela(); //To change body of generated methods, choose Tools | Templates.
-    if(getPintu()>=2 && getJendela()>=1){
-            System.out.println("Sesuai");
+
+        
+    
+
+    public int getPintu() {
+        if(pintu>=2 ){
+            System.out.println("Pintu="+pintu+" Sesuai"); 
             
         }
         else {
-            System.out.println("Tidak Sesuai");
+            System.out.println("Pintu="+pintu+" Tidak Sesuai");
         }
+        return pintu;
     }
+
+    public String setPintu(int pintu) {
+        this.pintu = pintu;
+        if(pintu>=2 ){
+            return "Sesuai";
+            
+        }
+        else {
+            return "Tidak Sesuai";
+    }
+    }
+
+    public int getJendela() {
+        if(jendela>=2 ){
+            System.out.println("Jendela:"+jendela+":Sesuai"); 
+            
+        }
+        else {
+            System.out.println("Jendela:"+jendela+":Tidak Sesuai");
+        }
+        
+        return jendela;
+    }
+
+    public String setJendela(int jendela) {
+        this.jendela = jendela;
+        if(jendela >=1){
+            return "Sesuai";
+            
+        }
+        else {
+            return "Tidak Sesuai";
+    }
+        
+    }
+    
+    abstract void Luas();
+    abstract void Rasio();
+    abstract void PintuJendela();
+    abstract void OutputRuangKelas();
     
     
 }
