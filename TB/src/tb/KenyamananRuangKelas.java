b08e0da1896af96e2bc4eedfff036ff49db999d4
MuhammadRiadi <mriadi38@gmail.com>
2015-06-06 09:19:18 +0700

    Muhammad Riadi

--- TB/src/tb/KenyamananRuangKelas.java
+++ TB/src/tb/KenyamananRuangKelas.java
@@ -1,86 +1,45 @@
 
 
 
-package riadi;
+package finaltb;
 
-import java.util.Scanner;
-public class KenyamananRuangKelas {
-    Scanner scan=new Scanner(System.in);
-        private int bising;
-        private int bau;
-        private int kebocoran;
-        private int kerusakan;
-        private int keausan;
-        
-    public int getBising() {
-        return bising;
-    }
 
-    public void setBising(int bising) {
-        this.bising = bising;
-    }
+public class KenyamananRuangKelas extends AnalisisKenyamananRuangKelas {
 
-    public int getBau() {
-        return bau;
-    }
-
-    public void setBau(int bau) {
-        this.bau = bau;
-    }
-
-    public int getKebocoran() {
-        return kebocoran;
-    }
-
-    public void setKebocoran(int kebocoran) {
-        this.kebocoran = kebocoran;
-    }
-
-    public int getKerusakan() {
-        return kerusakan;
-    }
-
-    public void setKerusakan(int kerusakan) {
-        this.kerusakan = kerusakan;
-    }
-
-    public int getKeausan() {
-        return keausan;
-    }
-
-    public void setKeausan(int keausan) {
-        this.keausan = keausan;
+    public KenyamananRuangKelas(int bising, int bau, int kebocoran, int kerusakan, int keausan) {
+        super(bising, bau, kebocoran, kerusakan, keausan);
     }
     
-    
-    
-    public void Bising(){
+     @Override
+    void Bising(){
         System.out.println("\n\n==KENYAMANAN RUANG KELAS==\n\n");
         System.out.println("Pilih Kebisingan Ruang Kelas");
         System.out.println("1.Bising\n2.Tidak Bising\nPilih [1-2]");
         setBising(scan.nextInt());
     }
-        
-        public void bau(){   
+     @Override   
+    void Bau(){   
         
         System.out.println("Apakah Ruang Kelas Anda Bau");
         System.out.println("1.Bau\n2.Tidak Bau\nPilih [1-2]");
         setBau(scan.nextInt());
         }
-        public void Kebocoran(){
+    @Override
+      void Kebocoran(){
         
         System.out.println("Apakah Ada Kebocoran");
         System.out.println("1.Ada\n2.Tidak Ada\nPilih [1-2]");
         setKebocoran(scan.nextInt());
         }
-        
-        public void Kerusakan(){
+     @Override   
+      void Kerusakan(){
             
         System.out.println("Apakah Ada Kerusakan");
         System.out.println("1.Ada\n2.Tidak Ada\nPilih [1-2]");
         setKerusakan(scan.nextInt());
         }
-        public void Keausan(){
+      @Override
+         void Keausan(){
         
         System.out.println("Apakah Ada Keausan");
         System.out.println("1.Ada\n2.Tidak Ada\nPilih [1-2]");
@@ -88,39 +47,20 @@ public class KenyamananRuangKelas {
         
     }
     
-    public void OutKenyamanan(){
+     @Override
+     void OutKenyamanan(){
         System.out.println("==KENYAMANAN RUANG KELAS==");
-        switch(getBising()){
-            case 1: System.out.println("Kebisingan = Iya");
-                break;
-            case 2: System.out.println("Kebisingan = Tidak ");
-                
-        }
-        switch(getBau()){
-            case 1: System.out.println("Bau = Iya");
-                break;
-            case 2: System.out.println("Bau = Tidak ");
-        break;
-    }
-        switch(getKebocoran()){
-            case 1: System.out.println("Kebocoran = Iya");
-                break;
-            case 2: System.out.println("Kebocoran = Tidak ");
-                break;
-        }   
-        switch(getKerusakan()){
-            case 1: System.out.println("Kerusakan = Iya");
-                break;
-            case 2: System.out.println("Kerusakan= Tidak ");
-                break;
-    }
-            switch(getKeausan()){
-            case 1: System.out.println("Keausan = Iya");
-                break;
-            case 2: System.out.println("Keausan = Tidak ");
-                break;
-            }
+        super.getBising();
+        super.getBau();
+        super.getKerusakan();
+        super.getKebocoran();
+        super.getKeausan();
+        
+        
+        
+        
     
         
     }
+
 }
