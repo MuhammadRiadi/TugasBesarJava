6b8174fd666b0b4bb34a411d7bd8a17957f97183
haisabdillah <haismansyahh@gmail.com>
2015-06-06 08:30:13 +0700

    Update KondisiPosisiSarana.java

--- TB/src/tb/KondisiPosisiSarana.java
+++ TB/src/tb/KondisiPosisiSarana.java
@@ -3,204 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hais;
-import java.util.Scanner;
-public class KondisiPosisiSarana {
-    Scanner scan=new Scanner(System.in);
-    private int steker;
-    private int kondisisteker;
-    private int posisisteker;
-    private int lcd;
-    private int kondisilcd;
-    private int posisilcd;
-    private int lampu;
-    private int kondisilampu;
-    private int posisilampu;
-    private int kipas;
-    private int kondisikipas;
-    private int posisikipas;
-    private int ac;
-    private int kondisiac;
-    private int posisiac;
-    private int internet;
-    private int login;
-    private int kondisilogin;
-    private int cctv;
-    private int posisicctv;
-    private int kondisicctv;
+package finaltb;
 
-    public int getSteker() {
-        return steker;
-    }
+public class KondisiPosisiSarana extends AnalisisKondisiPosisiSarana {
+    
 
-    public void setSteker(int steker) {
-        this.steker = steker;
-    }
-
-    public int getKondisisteker() {
-        return kondisisteker;
-    }
-
-    public void setKondisisteker(int kondisisteker) {
-        this.kondisisteker = kondisisteker;
-    }
-
-    public int getPosisisteker() {
-        return posisisteker;
-    }
-
-    public void setPosisisteker(int posisisteker) {
-        this.posisisteker = posisisteker;
-    }
-
-    public int getLcd() {
-        return lcd;
-    }
-
-    public void setLcd(int lcd) {
-        this.lcd = lcd;
-    }
-
-    public int getKondisilcd() {
-        return kondisilcd;
-    }
-
-    public void setKondisilcd(int kondisilcd) {
-        this.kondisilcd = kondisilcd;
-    }
-
-    public int getPosisilcd() {
-        return posisilcd;
-    }
-
-    public void setPosisilcd(int posisilcd) {
-        this.posisilcd = posisilcd;
-    }
-
-    public int getLampu() {
-        return lampu;
-    }
-
-    public void setLampu(int lampu) {
-        this.lampu = lampu;
-    }
-
-    public int getKondisilampu() {
-        return kondisilampu;
-    }
-
-    public void setKondisilampu(int kondisilampu) {
-        this.kondisilampu = kondisilampu;
-    }
-
-    public int getPosisilampu() {
-        return posisilampu;
-    }
-
-    public void setPosisilampu(int posisilampu) {
-        this.posisilampu = posisilampu;
-    }
-
-    public int getKipas() {
-        return kipas;
-    }
-
-    public void setKipas(int kipas) {
-        this.kipas = kipas;
-    }
-
-    public int getKondisikipas() {
-        return kondisikipas;
-    }
-
-    public void setKondisikipas(int kondisikipas) {
-        this.kondisikipas = kondisikipas;
-    }
-
-    public int getPosisikipas() {
-        return posisikipas;
-    }
-
-    public void setPosisikipas(int posisikipas) {
-        this.posisikipas = posisikipas;
-    }
-
-    public int getAc() {
-        return ac;
-    }
-
-    public void setAc(int ac) {
-        this.ac = ac;
-    }
-
-    public int getKondisiac() {
-        return kondisiac;
-    }
-
-    public void setKondisiac(int kondisiac) {
-        this.kondisiac = kondisiac;
-    }
-
-    public int getPosisiac() {
-        return posisiac;
-    }
-
-    public void setPosisiac(int posisiac) {
-        this.posisiac = posisiac;
-    }
-
-    public int getInternet() {
-        return internet;
-    }
-
-    public void setInternet(int internet) {
-        this.internet = internet;
-    }
-
-    public int getLogin() {
-        return login;
-    }
-
-    public void setLogin(int login) {
-        this.login = login;
-    }
-
-    public int getKondisilogin() {
-        return kondisilogin;
-    }
-
-    public void setKondisilogin(int kondisilogin) {
-        this.kondisilogin = kondisilogin;
-    }
-
-    public int getCctv() {
-        return cctv;
-    }
-
-    public void setCctv(int cctv) {
-        this.cctv = cctv;
-    }
-
-    public int getPosisicctv() {
-        return posisicctv;
-    }
-
-    public void setPosisicctv(int posisicctv) {
-        this.posisicctv = posisicctv;
-    }
-
-    public int getKondisicctv() {
-        return kondisicctv;
-    }
-
-    public void setKondisicctv(int kondisicctv) {
-        this.kondisicctv = kondisicctv;
+    public KondisiPosisiSarana(int steker, int kondisisteker, int posisisteker, int lcd, int kondisilcd, int posisilcd, int lampu, int kondisilampu, int posisilampu, int kipas, int kondisikipas, int posisikipas, int ac, int kondisiac, int posisiac, int internet, int login, int kondisilogin, int cctv, int posisicctv, int kondisicctv) {
+        super(steker, kondisisteker, posisisteker, lcd, kondisilcd, posisilcd, lampu, kondisilampu, posisilampu, kipas, kondisikipas, posisikipas, ac, kondisiac, posisiac, internet, login, kondisilogin, cctv, posisicctv, kondisicctv);
     }
     
     
     
-    
-     void StopKontak(){
+    @Override
+     void Steker(){
         System.out.println("\n\n==JUMLAH KONDISI DAN POSISI PRASARANA==\n\n");
         System.out.println("Masukkan Jumlah Stop Kontak: ");
         setSteker(scan.nextInt());
@@ -213,7 +28,7 @@ public class KondisiPosisiSarana {
         
         
     }
-    
+    @Override
     void Lcd(){
         System.out.println("Masukkan Jumlah Kabel LCD: ");
         setLcd(scan.nextInt());
@@ -226,7 +41,7 @@ public class KondisiPosisiSarana {
         
         
     }
-    
+    @Override
     void Lampu(){
         System.out.println("Masukkan Jumlah Lampu: ");
         setLampu(scan.nextInt());
@@ -240,8 +55,8 @@ public class KondisiPosisiSarana {
         
         
     }
-   
-    void KipasAngin(){
+   @Override
+    void Kipas(){
         System.out.println("Masukkan Jumlah Kipas Angin: ");
         setKipas(scan.nextInt());
         System.out.println("Pilih Kondisi Kipas Angin: ");
@@ -253,7 +68,7 @@ public class KondisiPosisiSarana {
         
         
     }
-     
+     @Override
     void Ac(){
         System.out.println("Masukkan Jumlah AC: ");
         setAc(scan.nextInt());
@@ -266,16 +81,16 @@ public class KondisiPosisiSarana {
         
         
     }
-    
+    @Override
     void Internet(){
-        System.out.println("Pilih SSID= ");
+        System.out.println("Pilih SSID ");
         System.out.println("1.UMM Hotspot\n2.UMM Premium\n.3.Lab Informatika\nPilih [1-3]=");
         setInternet(scan.nextInt());
         System.out.println("Login\n1.Berhasil\n2.Tidak");
         setLogin(scan.nextInt());
        
     }
-        
+    @Override
     void Cctv(){
         System.out.println("Masukkan Jumlah CCTV: ");
         setCctv(scan.nextInt());
@@ -288,6 +103,19 @@ public class KondisiPosisiSarana {
         
         
         
-    }  
+    }
+    @Override
+    void OutPosisi(){
+       getSteker();getKondisisteker();getPosisisteker();
+        getLcd();getKondisilcd();getPosisilcd();
+        getLampu();getKondisilampu();getPosisilampu();
+        getKipas();getKondisikipas();getPosisikipas();
+        getAc();getKondisiac();getPosisiac();
+        getInternet();getLogin();
+        getCctv();getKondisicctv();getPosisicctv();
+      
+        
+        
+    }
     
 }
