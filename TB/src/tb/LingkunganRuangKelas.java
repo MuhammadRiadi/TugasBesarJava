2f45f1f747271b83d2dff939aa3901174e5d017a
MuhammadRiadi <mriadi38@gmail.com>
2015-06-11 07:48:19 +0700

    Muhammad Riadi

--- TB/src/tb/LingkunganRuangKelas.java
+++ TB/src/tb/LingkunganRuangKelas.java
@@ -5,10 +5,14 @@
  */
 package finaltb;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 
 public class LingkunganRuangKelas extends AnalisisLingkunganRuangKelas {
 
-    public LingkunganRuangKelas(int lantai, int dinding, int atap, int pintu, int jendela) {
+    public LingkunganRuangKelas(String lantai, String dinding, String atap, String pintu, String jendela) {
         super(lantai, dinding, atap, pintu, jendela);
     }
 
@@ -17,23 +21,23 @@ public class LingkunganRuangKelas extends AnalisisLingkunganRuangKelas {
         System.out.println("\n\n==LINGKUNGAN RUANG KELAS==\n\n");
         System.out.println("Pilih Kondisi Lantai");
         System.out.println("1.Bersih\n2.Tidak Bersih\nPilih [1-2]= ");
-        super.setLantai(scan.nextInt());
+        super.setLantai(scan.nextLine());
         
         System.out.println("Pilih Kondisi Dinding");
         System.out.println("1.Bersih\n2.Tidak Bersih\nPilih [1-2]= ");
-        super.setDinding(scan.nextInt());
+        super.setDinding(scan.nextLine());
         
         System.out.println("Pilih Kondisi Atap");
         System.out.println("1.Bersih\n2.Tidak Bersih\nPilih [1-2]= ");
-        super.setAtap(scan.nextInt());
+        super.setAtap(scan.nextLine());
         
         System.out.println("Pilih Kondisi Pintu");
         System.out.println("1.Bersih\n2.Tidak Bersih\nPilih [1-2]= ");
-        super.setPintu(scan.nextInt());
+        super.setPintu(scan.nextLine());
         
         System.out.println("Pilih Kondisi Jendela");
         System.out.println("1.Bersih\n2.Tidak Bersih\nPilih [1-2]= ");
-        super.setJendela(scan.nextInt());
+        super.setJendela(scan.nextLine());
         
        
     }
@@ -41,11 +45,22 @@ public class LingkunganRuangKelas extends AnalisisLingkunganRuangKelas {
     
     @Override
  void OutputLingkungan(){
-     super.getLantai();
-     super.getDinding();
-     super.getAtap();
-     super.getPintu();
-     super.getJendela();
+     try{
+            File data=new File("kjhjkkhk.txt");
+            PrintWriter pr = new PrintWriter(new FileWriter(data,true));
+            pr.println("Lantai="+getLantai());
+            pr.println("Dinding="+getDinding());
+            pr.println("Atap="+getAtap());
+            pr.println("Pintu="+getPintu());
+            pr.println("Jendela="+getJendela());
+            
+            pr.close();
+            
+            
+        }
+        catch(Exception l){
+            System.out.println("Salah");
+        }
      
  }
     
