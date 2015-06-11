253089274b2656d02dc7299bc40bfe9dd48f5b30
haisabdillah <haismansyahh@gmail.com>
2015-06-11 07:09:16 +0700

    Haismansyah Abdillah

--- TB/src/tb/IdentitasRuangKelas.java
+++ TB/src/tb/IdentitasRuangKelas.java
@@ -1,44 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finaltb;
 
-package tb;
+import java.util.Scanner;
 
+/**
+ *
+ * @author Predator
+ */
+abstract public class AnalisisLingkunganRuangKelas  {
+        Scanner scan=new Scanner(System.in);
+    private String lantai;
+    private String dinding;
+    private String atap;
+    private String pintu;
+    private String jendela;
 
-
-public class IdentitasRuangKelas extends AnalisisIdentitasRuangKelas {
- 
-
-    public IdentitasRuangKelas(String nama, String lokasi, String prodi) {
-        super(nama, lokasi, prodi);
+    public AnalisisLingkunganRuangKelas(String lantai, String dinding, String atap, String pintu, String jendela) {
+        this.lantai = lantai;
+        this.dinding = dinding;
+        this.atap = atap;
+        this.pintu = pintu;
+        this.jendela = jendela;
     }
-  
-    @Override  
-    void Identitas(){
-        System.out.println("\n\n==IDENTITAS RUANG KELAS==\n\n");
+    
+    
+
+    public String getLantai() {
+        if(lantai.equalsIgnoreCase("Bersih")){
+            return "Sesuai";
+        }
+        else{
+            return "Tidak Sesuai"; 
+    }
         
-        System.out.println("Masukkan Nama Anda: ");
-        super.setNama(scan.nextLine());
-        System.out.println("Masukkan Lokasi Ruang: ");
-        super.setLokasi(scan.nextLine());
-        System.out.println("Masukkan Program Studi: ");
-        super.setProdi(scan.nextLine());
-}
-    @Override
-     void OutIdentitas(){
-         
-        super.getNama();
-        super.getLokasi();
-        super.getProdi();
     }
-     
-     
-     
-     
+
+    public void setLantai(String lantai) {
+        this.lantai = lantai;
+       
+    }
+
+    public String getDinding() {
+         if(dinding.equalsIgnoreCase("Bersih")){
+            return "Sesuai";
+        }
+        else{
+            return "Tidak Sesuai"; 
+    }
+    }
+
+    public void setDinding(String dinding) {
+        this.dinding = dinding;
+        
+    }
+
+    public String getAtap() {
+        
+     if(atap.equalsIgnoreCase("Bersih")){
+            return "Sesuai";
+        }
+        else{
+            return "Tidak Sesuai"; 
+    }
+    }
+
+    public void setAtap(String atap) {
+        this.atap = atap;
+    }
+
+    public String getPintu() {
+        if(pintu.equalsIgnoreCase("Bersih")){
+            return "Sesuai";
+        }
+        else{
+            return "Tidak Sesuai"; 
+    }
+    }
+
+    public void setPintu(String pintu) {
+        this.pintu = pintu;
+        
+    }
+
+    public String getJendela() {
+         if(jendela.equalsIgnoreCase("Bersih")){
+            return "Sesuai";
+        }
+        else{
+            return "Tidak Sesuai"; 
+         }
+    }
+
+    public void setJendela(String jendela) {
+        this.jendela = jendela;
+        
+    }
+
+    
+
+   abstract void InputLingkungan();
+        
+   abstract void OutputLingkungan();
+    
     
 }
-*\
-
-    
-
-    
-
-
-
